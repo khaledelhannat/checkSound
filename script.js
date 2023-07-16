de7b1f0b7106c74452bff35db20cb4a1d117e18b
Khaled Elhannat <khaled.waleed1631@gmail.com>
2023-07-16 15:18:37 +0300

    Adding the main JavaScript file

---
+++ script.js
@@ -0,0 +1,28 @@
+const sounds = ['strangeGorilla', 'slurpingNoodles', 'dice', 'chimes', 'dirtBikes', 'sippingCoffee',]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', ()=> {
+        stopSongs()
+
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+
+})
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0
+    })
+}
